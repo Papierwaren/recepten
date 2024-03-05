b41f4d77555b1e14071ed4ed9768291424db1731
Papierwaren <131025@stanislascollege.net>
2024-03-05 19:36:59 +0100

    CSS voor recepten.php

--- Recepten/Recepten.php
+++ Recepten/Recepten.php
@@ -19,7 +19,7 @@ require_once '../connect.php';
 
     <!--De navigatie balk-->
     <nav>
-    <div class="logo">
+      <div class="logo">
         <img src="../Images/NoordlesIcon.png" alt="Het Logo">
       </div>
 
@@ -33,17 +33,39 @@ require_once '../connect.php';
     </nav>
 
     <div class="Container">
+      <!-- Toegevoegd keuzemenu -->
+      <form method="get" action="Recepten.php" id="filter-form">
+        <select name="soort" id="soort">
+          <option value="alle">Alle</option>
+          <option value="ontbijt">Ontbijt</option>
+          <option value="lunch">Lunch</option>
+          <option value="diner">Diner</option>
+          <option value="dessert">Dessert</option>
+          <option value="snack">Snack</option>
+          <option value="overige">Overige</option>
+        </select>
+        <input type="submit" value="Filteren">
+      </form>
+
       <!--De kop-->
       <header>
         <h1>Recepten</h1>
       </header>
 
       <?php
+      // Lees de geselecteerde soort uit de URL
+      $geselecteerdeSoort = isset($_GET['soort']) ? $_GET['soort'] : 'alle';
+
       // Query om recepten op te halen met JOIN-operatie
       $sql = "SELECT recept.naam, soort.Gerecht, recept.beschrijving, recept.ingredienten, 
               recept.bereiding, recept.tijd, recept.aantal_pers 
               FROM recept 
               INNER JOIN soort ON recept.soort_id = soort.soort_id";
+
+      // Voeg een WHERE-clausule toe als een specifieke soort is geselecteerd
+      if ($geselecteerdeSoort !== 'alle') {
+        $sql .= " WHERE soort.Gerecht = '$geselecteerdeSoort'";
+      }
 
       $resultaat = $conn->query($sql);
 
@@ -69,7 +91,8 @@ require_once '../connect.php';
 
     <!--De balk aan de onderkant van het scherm-->
     <div class="footer">
-    <footer><ul>
+      <footer>
+        <ul>
           <li>
             <p>
               <span style="color:#ce2b37">Web</span><span style="color:#ffffff">cep</span><span style="color:#009246">tie</span>
@@ -77,7 +100,8 @@ require_once '../connect.php';
           </li>
           <li><a href="../Footer/Contact.php">Contact</a></li>
           <li><a href="../Footer/Voorwaarden.php">Algemene voorwaarden</a></li>
-        </ul></footer>
+        </ul>
+      </footer>
     </div>
   </div>
 </body>
