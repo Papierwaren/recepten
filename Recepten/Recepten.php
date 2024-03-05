8b72f203e4d0523da1daa433f44b9bd1ff08cfa3
Papierwaren <131025@stanislascollege.net>
2024-03-05 18:55:15 +0100

    Recepten tonen op recepten.php

--- Recepten/Recepten.php
+++ Recepten/Recepten.php
@@ -1,3 +1,8 @@
+<?php
+// Inclusief de databaseverbinding
+require_once '../connect.php';
+?>
+
 <!DOCTYPE html>
 <html lang="nl">
 
@@ -14,17 +19,17 @@
 
     <!--De navigatie balk-->
     <nav>
-        <div class="logo">
-            <img src="../Images/NoordlesIcon.png" alt="Het Logo">
-        </div>
+    <div class="logo">
+        <img src="../Images/NoordlesIcon.png" alt="Het Logo">
+      </div>
 
-        <div class="navigation">
-          <a href="../index.php">Home</a>
-          <a href="../Post/Post.php">Post</a>
-          <a href="Recepten.php">Recepten</a>
-        </div>
+      <div class="navigation">
+        <a href="../index.php">Home</a>
+        <a href="../Post/Post.php">Post</a>
+        <a href="Recepten.php">Recepten</a>
+      </div>
 
-        <a href="../Login/Login.php" class="icon"><img src="../Images/LoginIcon.png" alt="Inlog"></a>
+      <a href="../Login/Login.php" class="icon"><img src="../Images/LoginIcon.png" alt="Inlog"></a>
     </nav>
 
     <div class="Container">
@@ -32,22 +37,47 @@
       <header>
         <h1>Recepten</h1>
       </header>
-  
+
+      <?php
+      // Query om recepten op te halen met JOIN-operatie
+      $sql = "SELECT recept.naam, soort.Gerecht, recept.beschrijving, recept.ingredienten, 
+              recept.bereiding, recept.tijd, recept.aantal_pers 
+              FROM recept 
+              INNER JOIN soort ON recept.soort_id = soort.soort_id";
+
+      $resultaat = $conn->query($sql);
+
+      // Controleer of er resultaten zijn
+      if ($resultaat) {
+        // Loop door de resultaten en toon de gegevens
+        while ($row = $resultaat->fetch_assoc()) {
+          echo '<div class="recept">';
+          echo '<h2>' . $row['naam'] . '</h2>';
+          echo '<p>Beschrijving: ' . $row['beschrijving'] . '</p>';
+          echo '<p>Soort: ' . $row['Gerecht'] . '</p>';
+          echo '<p class="extra-info">Ingredienten: ' . $row['ingredienten'] . '</p>';
+          echo '<p class="extra-info">Bereiding: ' . $row['bereiding'] . '</p>';
+          echo '<p class="extra-info">Tijd: ' . $row['tijd'] . '</p>';
+          echo '<p class="extra-info">Aantal personen: ' . $row['aantal_pers'] . '</p>';
+          echo '</div>';
+        }
+      } else {
+        echo "Fout bij het ophalen van recepten: " . $conn->error;
+      }
+      ?>
     </div>
 
     <!--De balk aan de onderkant van het scherm-->
     <div class="footer">
-    <footer>
-        <ul>
+    <footer><ul>
           <li>
-          <p>
-                <span style="color:#ce2b37">Web</span><span style="color:#ffffff">cep</span><span style="color:#009246">tie</span>
+            <p>
+              <span style="color:#ce2b37">Web</span><span style="color:#ffffff">cep</span><span style="color:#009246">tie</span>
             </p>
           </li>
           <li><a href="../Footer/Contact.php">Contact</a></li>
-          <li><a href="../Footer/Voorwaarden.php">Algemene voorwaarden</a </li>
-        </ul>
-      </footer>
+          <li><a href="../Footer/Voorwaarden.php">Algemene voorwaarden</a></li>
+        </ul></footer>
     </div>
   </div>
 </body>
